9c3679561fb81c7e146d617fa4ea6ea25e6ffbff
LinneaAjger <linnea.ajger@gmail.com>
2022-12-16 15:26:05 +0100

    contact component updated

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -2,8 +2,12 @@ import React from 'react'
 import { StyledDiv } from './styles/DivStyles'
 
 const Contact = () => {
-  return (
-    <StyledDiv>Contact</StyledDiv>
+  return ( 
+  <StyledDiv>
+    <h3>Get in <span>touch!</span></h3>
+    <p>Do you have any questions or suggestions? Or perhaps just want to say hi? You are always welcome to contact us using the form below!
+    </p>
+  </StyledDiv>
   )
 }
 
